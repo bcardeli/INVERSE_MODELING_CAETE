39c7c3e18bb0cea4149761a7c9ea5ac32aa9f8c3
bcardeli <barbara.r.cardeli@gmail.com>
2023-11-06 18:45:20 -0300

    Update parameters to sensi_test RCP2.4

--- src/parameters.py
+++ src/parameters.py
@@ -2,11 +2,11 @@
 from pathlib import Path
 
 # Name of the base historical observed run.
-BASE_RUN = 'TESTE_SE' #"HISTORICAL-RUN" <- in sombrero this is the 
+BASE_RUN = 'nclim_SEcen' #"HISTORICAL-RUN" <- in sombrero this is the 
                   # STANDARD name for the historical observed run
 
 ATTR_FILENAME = "pls_attrs-1000.csv"
-START_COND_FILENAME = f"CAETE_STATE_START_TESTE_SE_.pkz"
+START_COND_FILENAME = f"CAETE_STATE_START_nclim_SEcen_.pkz"
 
 run_path = Path(f"../outputs/{BASE_RUN}/{START_COND_FILENAME}")
 pls_path = Path(f"../outputs/{BASE_RUN}/{ATTR_FILENAME}")
