8158e756fe86c875f9faabafbfbc09fd96ebb989
bcardeli <barbara.r.cardeli@gmail.com>
2023-11-07 15:18:39 -0300

    RCP2.4 - Update datas

--- src/parameters.py
+++ src/parameters.py
@@ -2,11 +2,11 @@
 from pathlib import Path
 
 # Name of the base historical observed run.
-BASE_RUN = 'nclim_SEcen' #"HISTORICAL-RUN" <- in sombrero this is the 
+BASE_RUN = 'nclim_central' #"HISTORICAL-RUN" <- in sombrero this is the 
                   # STANDARD name for the historical observed run
 
 ATTR_FILENAME = "pls_attrs-1000.csv"
-START_COND_FILENAME = f"CAETE_STATE_START_nclim_SEcen_.pkz"
+START_COND_FILENAME = f"CAETE_STATE_START_nclim_central_.pkz"
 
 run_path = Path(f"../outputs/{BASE_RUN}/{START_COND_FILENAME}")
 pls_path = Path(f"../outputs/{BASE_RUN}/{ATTR_FILENAME}")
