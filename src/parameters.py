901464970bddf6bc2051c22a0b5c88afcf743c19
bcardeli <barbara.r.cardeli@gmail.com>
2024-02-19 15:13:52 +0100

    RCP4.5 experiment (central grid)

--- src/parameters.py
+++ src/parameters.py
@@ -6,7 +6,7 @@ BASE_RUN = 'nclim_isa' #"HISTORICAL-RUN" <- in sombrero this is the
                   # STANDARD name for the historical observed run
 
 ATTR_FILENAME = "pls_attrs-1000.csv"
-START_COND_FILENAME = f"CAETE_STATE_START_nclim_isa_.pkz"
+START_COND_FILENAME = f"CAETE_STATE_START_nclim_central_.pkz"
 
 run_path = Path(f"../outputs/{BASE_RUN}/{START_COND_FILENAME}")
 pls_path = Path(f"../outputs/{BASE_RUN}/{ATTR_FILENAME}")
