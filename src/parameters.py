90c763fc818ef17c23bf0143a84be2b985024300
bcardeli <barbara.r.cardeli@gmail.com>
2024-02-20 17:03:34 +0100

    RCP7 Experiment (south)

--- src/parameters.py
+++ src/parameters.py
@@ -2,11 +2,11 @@
 from pathlib import Path
 
 # Name of the base historical observed run.
-BASE_RUN = 'nw_nclim' #"HISTORICAL-RUN" <- in sombrero this is the 
+BASE_RUN = 'nclim_south' #"HISTORICAL-RUN" <- in sombrero this is the 
                   # STANDARD name for the historical observed run
 
 ATTR_FILENAME = "pls_attrs-1000.csv"
-START_COND_FILENAME = f"CAETE_STATE_START_nw_nclim_.pkz"
+START_COND_FILENAME = f"CAETE_STATE_START_nclim_south_.pkz"
 
 run_path = Path(f"../outputs/{BASE_RUN}/{START_COND_FILENAME}")
 pls_path = Path(f"../outputs/{BASE_RUN}/{ATTR_FILENAME}")
